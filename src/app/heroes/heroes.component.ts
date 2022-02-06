74abadb806d7290d4a0a414316dab33b206c2c03
Brandon Barrett <btbarret@asu.edu>
2022-02-05 22:59:18 -0700

    Finished phase 4 of Angular Tour of Heroes

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,25 +1,40 @@
 import { Component, OnInit } from '@angular/core';
+
 import { Hero } from './../hero';
-import { HEROES } from './../mock-heroes';
+import { HeroService } from './../hero.service';
+import { MessageService } from './../message.service';
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
-export class HeroesComponent implements OnInit {
 
-  heroes: Hero[] = HEROES;
+export class HeroesComponent implements OnInit 
+{
+
+  heroes: Hero[] = [];
   selectedHero?: Hero;
 
-  constructor() { }
+  constructor(private heroService: HeroService, private messageService: MessageService) { }
 
+  /* On Select */
+  // Checks which heroe the user has clicked on from the list provided
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
-    console.log("Selected Hero: " + this.selectedHero.name);
+    this.messageService.add("Selected hero: " + hero.name);
+  }
+
+  /* Get Heroes */
+  // Calls HeroService to retrieve the list of heroes
+  getHeroes(): void {
+    this.heroService.getHeroes()
+      .subscribe(heroes => this.heroes = heroes);
   }
 
   ngOnInit(): void {
+    // Initialize list of heroes from service
+    this.getHeroes();
   }
 
 }
---
+++ src/app/message.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MessageService 
+{
+  messages: string[] = [];
+
+  constructor() { }
+
+  /* Add */
+  // Adds a string message to the list of all string messages
+  add(message: string) {
+    this.messages.push(message);
+  }
+
+  /* Clear */
+  // Empties the list of strings in messages
+  clear() {
+    this.messages = [];
+  }
+}
